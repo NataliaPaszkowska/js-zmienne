965d5e966a434aa5010a1536c63c21f9b6b2b7a1
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-10 09:48:58 +0200

    commit

---
+++ js/zmienne.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var imie;
+var imieDamskie = "Grazyna";
+var wiek = "20";
+var kolor = "czerwony";
+
+console.log(imie);
+console.log(wiek);
+console.log(kolor);
+
+imie = "Sergio";
+
+imie = imieDamskie;
+
+console.log(imie);
+console.log(wiek);
+console.log(kolor);
